92be65afa87c08e34140ad36899890949d30b62e
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-05-29 08:57:50 -0300

    :tada: Commit Inicial

---
+++ src/main/java/com/generation/rh_backend/RhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.generation.rh_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/rh_backend/RhBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.rh_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
