f5a548d3b43d9db98cf8a14b018f91d8590cd113
Helena <maria.helen@live.com>
2025-05-29 09:58:36 -0300

    :sparkles: Criação da Classe Controller

--- src/main/java/com/generation/rh_backend/controller/ColaboradoresController.java
+++ src/main/java/com/generation/rh_backend/controller/ColaboradoresController.java
@@ -1,16 +1,13 @@
 package com.generation.rh_backend.controller;
 
-import java.util.Optional;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import com.generation.rh_backend.model.Colaboradores;
+import com.generation.rh_backend.repository.ColaboradoresRepository;
 
 import jakarta.validation.Valid;
 
@@ -19,6 +16,37 @@ import jakarta.validation.Valid;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ColaboradoresController {
 
+    @Autowired
+    private ColaboradoresRepository colaboradoresRepository;
 
-	
+    //lsitar todos os colaboradores
+    @GetMapping
+    public ResponseEntity<List<Colaboradores>> getAll() {
+        return ResponseEntity.ok(colaboradoresRepository.findAll());
+    }
+
+    //buscar por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Colaboradores> getById(@PathVariable Long id) {
+        return colaboradoresRepository.findById(id)
+                .map(resp -> ResponseEntity.ok(resp))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    //criar novo colaborador
+    @PostMapping
+    public ResponseEntity<Colaboradores> post(@Valid @RequestBody Colaboradores colaborador) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(colaboradoresRepository.save(colaborador));
+    }
+
+    //colaborador existente
+    @PutMapping
+    public ResponseEntity<Colaboradores> put(@Valid @RequestBody Colaboradores colaborador) {
+        if (colaborador.getId() == null || !colaboradoresRepository.existsById(colaborador.getId()))
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(colaboradoresRepository.save(colaborador));
+    }
 }
