a370271c9da31ddea635bfb35ac3a9272ff4d8c9
JovaniOUnico <jovani.souza.almeida@gmail.com>
2025-05-29 09:31:18 -0300

    Subindo entidade Colaboradores

---
+++ src/main/java/com/generation/rh_backend/repository/ColaboradoresRepository.java
@@ -0,0 +1,9 @@
+package com.generation.rh_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.rh_backend.model.Colaboradores;
+
+public interface ColaboradoresRepository extends JpaRepository<Colaboradores, Long>{
+
+}
