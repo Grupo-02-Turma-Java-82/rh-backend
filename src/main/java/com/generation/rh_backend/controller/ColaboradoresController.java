66c07f431fc6012baddd6189979a68cb379b8160
Helena <maria.helen@live.com>
2025-05-29 11:43:11 -0300

    Alterações na classe Colaboradores Controller

--- src/main/java/com/generation/rh_backend/controller/ColaboradoresController.java
+++ src/main/java/com/generation/rh_backend/controller/ColaboradoresController.java
@@ -1,21 +1,21 @@
 package com.generation.rh_backend.controller;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.*;
-
-import com.generation.rh_backend.model.Colaboradores;
-import com.generation.rh_backend.repository.ColaboradoresRepository;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.rh_backend.model.Colaboradores;
+import com.generation.rh_backend.repository.ColaboradoresRepository;
 
 import jakarta.validation.Valid;
 
@@ -24,37 +24,57 @@ import jakarta.validation.Valid;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ColaboradoresController {
 
-    @Autowired
-    private ColaboradoresRepository colaboradoresRepository;
+	@Autowired
+	private ColaboradoresRepository colaboradoresRepository;
 
-    //lsitar todos os colaboradores
-    @GetMapping
-    public ResponseEntity<List<Colaboradores>> getAll() {
-        return ResponseEntity.ok(colaboradoresRepository.findAll());
-    }
+	// listar todos os colaboradores
+	@GetMapping
+	public ResponseEntity<List<Colaboradores>> getAll() {
+		return ResponseEntity.ok(colaboradoresRepository.findAll());
+	}
 
-    //buscar por ID
-    @GetMapping("/{id}")
-    public ResponseEntity<Colaboradores> getById(@PathVariable Long id) {
-        return colaboradoresRepository.findById(id)
-                .map(resp -> ResponseEntity.ok(resp))
-                .orElse(ResponseEntity.notFound().build());
-    }
+	// buscar por ID
+	@GetMapping("/{id}")
+	public ResponseEntity<Colaboradores> getById(@PathVariable Long id) {
+		return colaboradoresRepository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	// buscar por nome
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Colaboradores>> getByNome(@PathVariable String nome) {
+	    List<Colaboradores> colaboradores = colaboradoresRepository.findAllByNomeContainingIgnoreCase(nome);
+	    if (colaboradores.isEmpty()) {
+	        return ResponseEntity.notFound().build();
+	    }
+	    return ResponseEntity.ok(colaboradores);
+	}
 
-    //criar novo colaborador
-    @PostMapping
-    public ResponseEntity<Colaboradores> post(@Valid @RequestBody Colaboradores colaborador) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(colaboradoresRepository.save(colaborador));
-    }
+	// buscar por email (opt)
+	@GetMapping("/email/{email}")
+	public ResponseEntity<Colaboradores> getByEmail(@PathVariable String email) {
+		return colaboradoresRepository.findAllByEmailContainingIgnoreCase(email)
+				.map(colaborador -> ResponseEntity.ok(colaborador)).orElse(ResponseEntity.notFound().build());
+	}
 
-    //colaborador existente
-    @PutMapping
-    public ResponseEntity<Colaboradores> put(@Valid @RequestBody Colaboradores colaborador) {
-        if (colaborador.getId() == null || !colaboradoresRepository.existsById(colaborador.getId()))
-            return ResponseEntity.notFound().build();
+	// buscar por telefone (opt)
+	@GetMapping("/telefone/{telefone}")
+	public ResponseEntity<Colaboradores> getByTelefone(@PathVariable String telefone) {
+		return colaboradoresRepository.findAllByTelefoneContainingIgnoreCase(telefone)
+				.map(colaborador -> ResponseEntity.ok(colaborador)).orElse(ResponseEntity.notFound().build());
+	}
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(colaboradoresRepository.save(colaborador));
-    }
+	// criar novo colaborador
+	@PostMapping
+	public ResponseEntity<Colaboradores> post(@Valid @RequestBody Colaboradores colaborador) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(colaboradoresRepository.save(colaborador));
+	}
+
+	// colaborador existente
+	@PutMapping
+	public ResponseEntity<Colaboradores> put(@Valid @RequestBody Colaboradores colaborador) {
+		if (colaborador.getId() == null || !colaboradoresRepository.existsById(colaborador.getId()))
+			return ResponseEntity.notFound().build();
+
+		return ResponseEntity.status(HttpStatus.OK).body(colaboradoresRepository.save(colaborador));
+	}
 }
